be7f9adf74aad26e4a421bcc2a1716056e1513a3
Almeida-221 <almeidatony2019@gmail.com>
2026-04-08 11:51:53 +0000

    description du changement

--- deploy.bat
+++ deploy.bat
@@ -3,22 +3,15 @@
 set MSG=%~1
 if "%MSG%"=="" set MSG=mise a jour
 
-echo === Commit + push backend...
+echo === Commit + push...
 cd C:\xampp\htdocs\sb
 git add -A
 git commit -m "%MSG%"
 git push origin main
 
-echo === Deploiement sur le serveur...
-ssh tony@180.149.196.39 "cd /var/www/html/sb && git pull origin main && rm -rf bootstrap/cache/*.php storage/framework/cache/data/* storage/framework/views/* && php artisan route:clear && echo Serveur OK"
-
-echo === Build APK Flutter...
-cd C:\Users\almei\sb_securite
-call flutter clean
-call flutter pub get
-call flutter build apk --release
+echo === Deploiement serveur...
+ssh tony@180.149.196.39 "cd /var/www/html/sb && git pull origin main && rm -rf bootstrap/cache/*.php storage/framework/cache/data/* storage/framework/views/* && php artisan route:clear && echo OK"
 
 echo.
-echo Deploy termine !
-echo APK : C:\Users\almei\sb_securite\build\app\outputs\flutter-apk\app-release.apk
+echo Done !
 pause
